d03ffd0e9a219e88aef79084824866572af8c873
Roman Turivnyi <r.turivnyi@foodtech.team>
2023-06-24 23:31:23 +0300

    add sidebar.js

--- src/pages/sidebar/Slidebar.js
+++ src/pages/sidebar/Slidebar.js
@@ -1,9 +1,18 @@
 import './Slidebar.css'
+import materials from '../../data/materials.json'
 
 function Slidebar(){
     return(
         <div className='Slidebar'>
-
+            <nav>
+                <ul>
+                    {materials.map((item, idx)=>
+                        <li key={idx}>
+                            <a href={item.href}>{item.title}</a>    
+                        </li>
+                    )}
+                </ul>
+            </nav>
         </div>
     );
 }
